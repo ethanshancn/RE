29fcd341868e82415d1a3fa91cf8b2763bb8504d
Shan <silams@sina.com>
2015-12-31 14:13:09 +0800

    dev

--- main.js
+++ main.js
@@ -54,7 +54,6 @@ cc.game.onStart = function(){
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new HomeScene());
-        //cc.director.runScene(new RankingListSence());
     }, this);
 };
 cc.game.run();
